22f545aecbda0ba99e4409430bf30687bb99c25e
Kyle Conroy <kyle@kyleconroy.com>
2017-03-17 20:38:14 -0700

    Initial Commit

---
+++ Area51/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Area51
+//
+//  Created by Kyle Conroy on 3/17/17.
+//  Copyright © 2017 Stack Machine Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
